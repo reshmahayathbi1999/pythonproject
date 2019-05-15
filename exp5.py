b6419f402020159d51f78cef79d445c9f30eb855
reshmahayathbi1999 <50619287+reshmahayathbi1999@users.noreply.github.com>
2019-05-15 08:29:31 +0530

    Update exp5.py

--- exp5.py
+++ exp5.py
@@ -1,6 +1,7 @@
-num1=int(input('enter the number'))
-num2=int(input('enter the number'))
-num3=int(input('enter the number'))
+num1,num2,num3=input().split()
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
 if((num1>num2) and (num1>num3)):
     print(num1)
 elif((num2>num1) and (num2>num3)):
