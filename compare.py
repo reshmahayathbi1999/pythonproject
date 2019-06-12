98d3304c8c40c228d7bf0a92b72d2b453145d3a2
reshmahayathbi1999 <50619287+reshmahayathbi1999@users.noreply.github.com>
2019-06-12 22:24:45 +0530

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,10 @@
+string,num=input().split()
+string=str(string)
+num=str(num)
+if(len(string)>len(num)):
+    print(string)
+elif (len(num)>len(string)):
+    print(num)
+else:
+    print(string)
+    
