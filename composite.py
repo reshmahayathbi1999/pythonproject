125b4c9f44648ec60dd9605613a38dd8259e88ef
reshmahayathbi1999 <50619287+reshmahayathbi1999@users.noreply.github.com>
2019-06-28 01:13:52 +0530

    Update composite.py

--- composite.py
+++ composite.py
@@ -1,7 +1,7 @@
-num = int(input())
-if num > 1:
-    for i in range(2, num):
-        if (num % i) == 0:
+num1 = int(input())
+if num1 > 1:
+    for i in range(2, num1):
+        if (num1 % i) == 0:
             print("yes")
             break
     else:
